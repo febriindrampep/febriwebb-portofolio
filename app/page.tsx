c690d1ecc6fac3bf3597b39ba633e510a0570a9d
febri indra pratama <febriindrapratama2027@gmail.com>
2026-07-26 22:02:09 +0700

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
-import PortfolioGrid from "@/components/PortofolioGrid.tsx";
-import Layanan from "@/components/Layanan.tsx";
+import PortfolioGrid from "@/components/PortofolioGrid";
+import Layanan from "@/components/Layanan";
 
 export default function Home() {
   return (
